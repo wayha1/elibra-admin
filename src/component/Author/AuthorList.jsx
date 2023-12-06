b92add488e28f72f59d7ba1d198d4e22c83bfc81
Seng Wayha <vehaseng1122@gmail.com>
2023-12-06 13:09:48 +0700

    update

--- src/component/Author/AuthorList.jsx
+++ src/component/Author/AuthorList.jsx
@@ -18,12 +18,20 @@ export const AuthorList = () => {
   return (
     <div className="container w-auto">
       {authorList.map((author) => (
-        <div key={author.id}>
-          {author.authName} 
-          {author.Gender}
-          {author.decs}
-          {author.DOB}
-          <img src={author.imgAuth} />
+        <div key={author.id} className="flex items-center mb-4 p-4 bg-white rounded-lg">
+          <div className="">
+            <img src={author.imgAuth} alt={author.authName} className="w-40 h-50" />
+          </div>
+          <div className="ml-4">
+            <p className="text-lg font-bold">{author.authName}</p>
+            <p>{author.Gender}</p>
+            <p>{author.decs}</p>
+            <p>{author.DOB}</p>
+          </div>
+          <div className="ml-auto flex">
+            <button className="mr-2 bg-red-500 text-white active:bg-blue-500 p-2 rounded">Delete</button>
+            <button className="bg-green-500 text-white p-2 active:bg-blue-500 rounded">Update</button>
+          </div>
         </div>
       ))}
     </div>
