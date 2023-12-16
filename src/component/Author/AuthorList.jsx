c249f6fa70cb0a95262976322edeb59a1ec47a4a
JackyDom <sopheaoudom8@gmail.com>
2023-12-16 20:22:09 +0700

    update

--- src/component/Author/AuthorList.jsx
+++ src/component/Author/AuthorList.jsx
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { collection, getDocs, doc, deleteDoc, updateDoc, setDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  doc,
+  deleteDoc,
+  updateDoc,
+  setDoc,
+} from "firebase/firestore";
 import { db, imgDB } from "../../firebase";
-import { ref, deleteObject, uploadBytes, getDownloadURL } from "firebase/storage";
+import {
+  ref,
+  deleteObject,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
 import Modal from "./Modal";
 import SeachAuthor from "./SeachAuthor";
 import { v4 as uuidv4 } from "uuid";
@@ -22,6 +34,9 @@ export const AuthorList = () => {
   const [fullAuthorList, setFullAuthorList] = useState([]);
   const [maleAuthorList, setMaleAuthorList] = useState([]);
   const [femaleAuthorList, setFemaleAuthorList] = useState([]);
+
+  // State variable for sorting order
+  const [sortOrder, setSortOrder] = useState("asc");
   const [updatedAuthor, setUpdatedAuthor] = useState({
     authName: "",
     Decs: "",
@@ -63,24 +78,25 @@ export const AuthorList = () => {
     try {
       const authorRef = doc(db, "Author", updatedAuthor.id);
       const imgRef = ref(imgDB, updatedAuthor.imgAuth);
-
-      // Initialize newData with the existing values
       const newData = {
         authName: updatedAuthor.authName,
         Gender: updatedAuthor.Gender,
         Decs: updatedAuthor.Decs,
-        authDOB: updatedAuthor.authDOB, 
-        imgAuth: updatedAuthor.imgAuth, 
+        authDOB: updatedAuthor.authDOB,
       };
+
       if (authImage) {
-        const newImgRef = ref(imgDB, `WebsiteProject/AboutUs/${authImage.name + uuidv4()}`);
+        const newImgRef = ref(
+          imgDB,
+          `WebsiteProject/AboutUs/${authImage.name + uuidv4()}`
+        );
         await uploadBytes(newImgRef, authImage);
         const newImgUrl = await getDownloadURL(newImgRef);
         newData.imgAuth = newImgUrl;
       }
-      const filteredData = Object.fromEntries(Object.entries(newData).filter(([_, v]) => v !== undefined));
-      await updateDoc(authorRef, filteredData);
-      setUpdateSuccess(true);
+
+      await updateDoc(authorRef, newData);
+      setUpdateSuccess(true); // Set updateSuccess to true on successful update
     } catch (error) {
       console.error("Error updating document or image:", error.message);
     } finally {
@@ -102,11 +118,15 @@ export const AuthorList = () => {
         ...doc.data(),
         id: doc.id,
       }));
-      authors.sort((a, b) => a.authName.localeCompare(b.authName));
       setFullAuthorList(authors);
-      setAuthorList(authors);
-      const maleAuthors = authors.filter((author) => author.Gender.toLowerCase() === "ប្រុស");
-      const femaleAuthors = authors.filter((author) => author.Gender.toLowerCase() === "ស្រី");
+      setAuthorList(authors); // Set the default list to the full list
+      // Separate male and female lists
+      const maleAuthors = authors.filter(
+        (author) => author.Gender.toLowerCase() === "ប្រុស"
+      );
+      const femaleAuthors = authors.filter(
+        (author) => author.Gender.toLowerCase() === "ស្រី"
+      );
 
       setMaleAuthorList(maleAuthors);
       setFemaleAuthorList(femaleAuthors);
@@ -128,12 +148,16 @@ export const AuthorList = () => {
         break;
     }
   };
-
   return (
     <div className="container w-auto">
       <SeachAuthor />
+      {/* Dropdown selector for sorting */}
+      {/* Dropdown selector for sorting */}
       <div className="flex mb-4 pr-4 w-full justify-end ">
-        <select className="p-2 border rounded-md " onChange={(e) => handleSort(e.target.value)}>
+        <select
+          className="p-2 border rounded-md "
+          onChange={(e) => handleSort(e.target.value)}
+        >
           <option value="">Sort : ទាំងអស់ (All)</option>
           <option value="ប្រុស">ប្រុស (Male)</option>
           <option value="ស្រី">ស្រី (Female)</option>
@@ -148,7 +172,11 @@ export const AuthorList = () => {
           onMouseEnter={() => setHoveredAuthor(author.id)}
           onMouseLeave={() => setHoveredAuthor(null)}
         >
-          <img src={author.imgAuth} alt={author.authName} className="w-40 h-50" />
+          <img
+            src={author.imgAuth}
+            alt={author.authName}
+            className="w-40 h-50"
+          />
           <div className="ml-8">
             <p className="text-lg font-bold font-title">{author.authName}</p>
             <p className="font-title">{author.Gender}</p>
@@ -194,7 +222,10 @@ export const AuthorList = () => {
           <div className="absolute inset-0 flex items-center justify-center ">
             <div className="bg-white p-4 rounded shadow-lg">
               <p className="mb-4">Delete successful!</p>
-              <button className="bg-gray-500 text-white p-2 rounded" onClick={() => setDeleteSuccess(false)}>
+              <button
+                className="bg-gray-500 text-white p-2 rounded"
+                onClick={() => setDeleteSuccess(false)}
+              >
                 Close
               </button>
             </div>
@@ -203,7 +234,9 @@ export const AuthorList = () => {
       )}
 
       {/* Update Modal */}
-      <div className={`fixed inset-0 z-30 ${updateModalOpen ? "block" : "hidden"}`}>
+      <div
+        className={`fixed inset-0 z-30 ${updateModalOpen ? "block" : "hidden"}`}
+      >
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="bg-white p-4 rounded shadow-lg">
@@ -211,7 +244,9 @@ export const AuthorList = () => {
 
             {/* Update input fields to allow user input */}
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">Name :</label>
+              <label className="block text-sm font-medium text-gray-700">
+                Name :
+              </label>
               <input
                 type="text"
                 className="mt-1 p-2 border rounded-md w-full"
@@ -225,27 +260,37 @@ export const AuthorList = () => {
               />
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">Gender :</label>
+              <label className="block text-sm font-medium text-gray-700">
+                Gender :
+              </label>
               <input
                 type="text"
                 className="mt-1 p-2 border rounded-md w-full"
                 value={updatedAuthor.Gender}
-                onChange={(e) => setUpdatedAuthor({ ...updatedAuthor, Gender: e.target.value })}
+                onChange={(e) =>
+                  setUpdatedAuthor({ ...updatedAuthor, Gender: e.target.value })
+                }
               />
             </div>
 
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">Description :</label>
+              <label className="block text-sm font-medium text-gray-700">
+                Description :
+              </label>
               <input
                 type="text"
                 className="mt-1 p-2 border rounded-md w-full"
                 value={updatedAuthor.Decs}
-                onChange={(e) => setUpdatedAuthor({ ...updatedAuthor, Decs: e.target.value })}
+                onChange={(e) =>
+                  setUpdatedAuthor({ ...updatedAuthor, Decs: e.target.value })
+                }
               />
             </div>
 
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">Date of Birth</label>
+              <label className="block text-sm font-medium text-gray-700">
+                Date of Birth
+              </label>
               <input
                 type="text"
                 className="mt-1 p-2 border rounded-md w-full"
@@ -270,7 +315,10 @@ export const AuthorList = () => {
               />
             </div>
             <div className="flex justify-end">
-              <button className="mr-2 bg-green-500 text-white p-2 rounded" onClick={() => confirmUpdate()}>
+              <button
+                className="mr-2 bg-green-500 text-white p-2 rounded"
+                onClick={() => confirmUpdate()}
+              >
                 Update
               </button>
               <button
@@ -295,7 +343,10 @@ export const AuthorList = () => {
           <div className="absolute inset-0 flex items-center justify-center ">
             <div className="bg-white p-4 rounded shadow-lg">
               <p className="mb-4">Update successful!</p>
-              <button className="bg-gray-500 text-white p-2 rounded" onClick={() => setUpdateSuccess(false)}>
+              <button
+                className="bg-gray-500 text-white p-2 rounded"
+                onClick={() => setUpdateSuccess(false)}
+              >
                 Close
               </button>
             </div>
@@ -304,27 +355,40 @@ export const AuthorList = () => {
       )}
 
       {/* Author Detail Modal */}
-      <div className={`fixed inset-0 z-50 ${authorDetailModalOpen ? "block" : "hidden"}`}>
+      <div
+        className={`fixed inset-0 z-50 ${
+          authorDetailModalOpen ? "block" : "hidden"
+        }`}
+      >
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="bg-white p-4 rounded shadow-xl mb-2">
             <h2 className="text-2xl font-bold mb-4">Author Detail</h2>
             <p className="flex text-xl font-bold ">
               Name:
-              <p className="flex ml-4 text-gray-700 hover:text-sky-800">{updatedAuthor.authName}</p>
+              <p className="flex ml-4 text-gray-700 hover:text-sky-800">
+                {updatedAuthor.authName}
+              </p>
             </p>
             <p className="flex text-xl font-bold ">
-              Gender: <p className="flex ml-4 text-gray-700">{updatedAuthor.Gender}</p>
+              Gender:{" "}
+              <p className="flex ml-4 text-gray-700">{updatedAuthor.Gender}</p>
             </p>
             <p className="flex text-xl font-bold ">
               Description:{" "}
-              <p className="flex ml-4 text-gray-700 text-lg subpixel-antialiased	">{updatedAuthor.Decs}</p>
+              <p className="flex ml-4 text-gray-700 text-lg subpixel-antialiased	">
+                {updatedAuthor.Decs}
+              </p>
             </p>
             <p className="flex text-xl font-bold ">
-              Date of Birth: <p className="flex ml-4 text-gray-700">{updatedAuthor.authDOB}</p>
+              Date of Birth:{" "}
+              <p className="flex ml-4 text-gray-700">{updatedAuthor.authDOB}</p>
             </p>
             <div className="flex w-full items-center justify-center ">
-              <img src={updatedAuthor.imgAuth} className="w-[500px] h-[500px] border-4 " />
+              <img
+                src={updatedAuthor.imgAuth}
+                className="w-[500px] h-[500px] border-4 "
+              />
             </div>
             <div className="flex justify-end mt-4">
               <button
--- src/component/Author/SeachAuthor.jsx
+++ src/component/Author/SeachAuthor.jsx
@@ -1,59 +1,130 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../firebase";
 
 const SearchAuthor = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  const [authorData, setAuthorData] = useState(null);
+  const [authorList, setAuthorList] = useState([]);
+  const [selectedAuthor, setSelectedAuthor] = useState(null);
+  const [searchIndex, setSearchIndex] = useState(0);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+  const dropdownClosedDueToSelection = useRef(false);
 
   useEffect(() => {
     const searchAuthor = async () => {
       try {
-        const authorsCollection = collection(db, "Author");
-        const q = query(authorsCollection, where("authName", "==", searchQuery));
-        const querySnapshot = await getDocs(q);
+        if (searchQuery === "") {
+          setAuthorList([]);
+          setShowDropdown(false);
+          return;
+        }
 
-        if (querySnapshot.docs.length === 1) {
-          const author = querySnapshot.docs[0].data();
-          setAuthorData(author);
+        const authorsCollection = collection(db, "Author");
+        let tempQuery = query(
+          authorsCollection,
+          where("authName", ">=", searchQuery.slice(0, searchIndex + 1)),
+          where("authName", "<=", searchQuery.slice(0, searchIndex + 1) + "\uf8ff")
+        );
+
+        if (!dropdownClosedDueToSelection.current) {
+          const querySnapshot = await getDocs(tempQuery);
+
+          if (querySnapshot.docs.length > 0) {
+            const authors = querySnapshot.docs.map((doc) => doc.data());
+            setAuthorList(authors);
+            setShowDropdown(true);
+          } else {
+            setAuthorList([]);
+            setShowDropdown(false);
+          }
         } else {
-          setAuthorData(null);
+          dropdownClosedDueToSelection.current = false;
         }
       } catch (error) {
-        console.error("Error searching for author:", error.message);
+        console.error("Error searching for authors:", error.message);
       }
     };
 
-    // Call the search function when the searchQuery changes
+    // Call the search function when the searchQuery or searchIndex changes
     searchAuthor();
-  }, [searchQuery]);
+  }, [searchQuery, searchIndex]);
+
+  const handleAuthorSelection = (author) => {
+    setSelectedAuthor(author);
+    setSearchQuery(author.authName);
+    setAuthorList([]);
+    setShowDropdown(false);
+    dropdownClosedDueToSelection.current = true;
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setAuthorList([]);
+      setShowDropdown(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setSearchQuery(value);
+    setSearchIndex(value.length - 1);
+  };
 
   return (
-    <div className="container mx-auto p-2">
-      <div className="my-2 flex">
+    <div className="container mx-auto p-2 relative">
+      <div className="my-2 flex relative">
         <input
           type="text"
           placeholder="Search author"
-          className="border p-2 rounded-md w-full"
+          className="border p-2 rounded-md w-full focus:ring focus:border-blue-300"
           value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
+          onChange={handleInputChange}
         />
-        <button className="ml-2 bg-blue-500 text-white p-2 rounded" onClick={() => SearchAuthor()}>
-          Search
+        {showDropdown && authorList.length > 0 && (
+          <div ref={dropdownRef} className="absolute bg-white border rounded mt-5 w-full shadow-md">
+            {authorList.map((author, index) => (
+              <div
+                key={author.authName}
+                className="p-2 cursor-pointer hover:bg-gray-200"
+                onClick={() => handleAuthorSelection(author)}
+              >
+                {`${index + 1}. ${author.authName}`}
+              </div>
+            ))}
+          </div>
+        )}
+        <button
+          className="ml-2 bg-blue-500 text-white p-2 rounded"
+          onClick={() => {
+            setSelectedAuthor(null);
+            setSearchQuery(""); // Clear the search input
+            setSearchIndex(0);
+            setShowDropdown(false);
+          }}
+        >
+          Clear
         </button>
       </div>
 
-      {authorData ? (
+      {selectedAuthor ? (
         <div className="border p-2 mt-2 bg-white rounded-lg flex items-center">
           <img
-            src={authorData.imgAuth}
-            alt={authorData.authName}
+            src={selectedAuthor.imgAuth}
+            alt={selectedAuthor.authName}
             className="w-48 h-40 object-cover mb-2 rounded"
           />
           <div className="ml-4">
-            <p className="text-lg font-bold">{authorData.authName}</p>
-            <p className="text-sm">{authorData.Gender}</p>
-            <p className="text-sm">{authorData.DOB}</p>
+            <p className="text-lg font-bold">{selectedAuthor.authName}</p>
+            <p className="text-sm">{selectedAuthor.Gender}</p>
+            <p className="text-sm">{selectedAuthor.DOB}</p>
             {/* Add other fields as needed */}
           </div>
         </div>
